8ef6bad4451824e37093527c8b41f963b54e6330
Maryna Petranova <74003292+marynaqed@users.noreply.github.com>
2021-05-17 18:17:47 +0300

    Update RegisterForm.tsx

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -16,7 +16,7 @@ export const RegisterForm: React.FC<Entry<RegistrationFormFields>> = ({
   const { tracker } = useUniformTracker();
 
   const onRegister = () => {
-    tracker.addEnrichment({name: 'color', value: red', str: 80});
+    tracker.addEnrichment({name: 'color', value: 'red', str: 80});
     //document.cookie = 'unfrmconf_registered=true; path=/; samesite=lax';
     tracker.reevaluateSignals();
     setRegistered(true);
